3a94d7ff4b3e94cb2d3db25a7522d25f0e0c92f2
EvansAbraham <jea150702@outlook.com>
2024-10-21 12:00:54 +0530

    db added

--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,49 @@
 import { v4 as uuid } from 'uuid';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-let users = [];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dataFilePath = path.join(__dirname, '../data/users.json');
+
+// Utility function to read users from the JSON file
+const readUsersFromFile = () => {
+    const data = fs.readFileSync(dataFilePath);
+    return JSON.parse(data);
+};
+
+// Utility function to write users to the JSON file
+const writeUsersToFile = (users) => {
+    fs.writeFileSync(dataFilePath, JSON.stringify(users, null, 2));
+};
 
 export const getUsers = (req, res) => {
+    const users = readUsersFromFile();
     console.log(`Users in the database: ${users}`);
-
     res.send(users);
-}
+};
 
 export const createUser = (req, res) => {   
     const user = req.body;
-
-    users.push({...user, id: uuid()});
+    const users = readUsersFromFile();
+    
+    const newUser = { ...user, id: uuid() };
+    users.push(newUser);
+    
+    writeUsersToFile(users);
     
     console.log(`User [${user.username}] added to the database.`);
-    console.log(users);
-    
-    res.status(201).send({ message: "User created", user });
+    res.status(201).send({ message: "User created", user: newUser });
 };
 
 export const getUser = (req, res) => {
+    const users = readUsersFromFile();
     const user = users.find((user) => user.id === req.params.id);
     
     if (user) {
-        res.send({id: user.id, username: user.username, age: user.age });
+        res.send({ id: user.id, username: user.username, age: user.age });
         console.log(`Users in the database: ${users}`);
     } else {
         res.status(404).send({ message: "User not found" });
@@ -31,21 +51,30 @@ export const getUser = (req, res) => {
 };
 
 export const deleteUser = (req, res) => { 
-    console.log(`user with id ${req.params.id} has been deleted`);
+    const users = readUsersFromFile();
+    const updatedUsers = users.filter((user) => user.id !== req.params.id);
     
-    users = users.filter((user) => user.id !== req.params.id);
+    if (updatedUsers.length === users.length) {
+        return res.status(404).send({ message: "User not found" });
+    }
+    
+    writeUsersToFile(updatedUsers);
+    console.log(`User with id ${req.params.id} has been deleted`);
+    
     res.send({ message: "User deleted successfully" });
 };
 
 export const updateUser = (req, res) => {
+    const users = readUsersFromFile();
     const user = users.find((user) => user.id === req.params.id);
     
     if (user) {
         user.username = req.body.username;
         user.age = req.body.age;
 
-        console.log(`Username has been updated to ${req.body.username}. Age has been updated to ${req.body.age}.`);
+        writeUsersToFile(users);
         
+        console.log(`Username has been updated to ${req.body.username}. Age has been updated to ${req.body.age}.`);
         res.send({ message: "User updated", user });
     } else {
         res.status(404).send({ message: "User not found" });
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-
 import usersRoutes from "./routes/users.js";
 
 const app = express();
@@ -10,6 +9,10 @@ app.use(bodyParser.json());
 
 app.use("/people", usersRoutes);
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
-app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+app.all("*", (req, res) => res.send("You've tried reaching a route that doesn't exist."));
 
-app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+}
+
+export default app;
